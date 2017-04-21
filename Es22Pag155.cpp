cf1fe6c607d2ad6fd09f2cc3f7fa1fdd5dde841a
silviapioveasan <annarita.talucci@alice.it>
2017-04-21 11:07:57 +0200

    Add files via upload

---
+++ Es22Pag155.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int v1[], v2[], dim1, dim2;
+
+    cout<<"inserisci la dimensione del primo vettore"<<endl;
+    cin>>dim1;
+    cout<<"inserisci la dimensione del secondo vettore"<<endl;
+    cin>>dim2;
+
+    do{
+        cout<<"inserisci l'elemento del vettore"<<endl;
+
+
+
+    }
+
+
+
+}
